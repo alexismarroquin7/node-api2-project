c5e3211c46c3c3c5bb360e1c53849e5b8962b1a4
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-06 12:21:27 -0700

    routes to get * posts && get post by id

--- api/server.js
+++ api/server.js
@@ -3,5 +3,9 @@
 const express = require("express");
 const server = express();
 
+const postRouter = require("./posts/posts-router");
+
+server.use(express.json());
+server.use('/api/posts', postRouter);
 
 module.exports = server;
